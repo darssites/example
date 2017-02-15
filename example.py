c87ea8fcf91f487ed028a9c7122e9cd0fda8dcb6
gusg21 <gusg21@parkschool.net>
2017-02-14 20:02:55 -0500

    Template completed

---
+++ example.py
@@ -0,0 +1,12 @@
+# The main module for the plugin.
+# Note: In ALL plugins there must be a python file called [name-of-plugin].py in a folder called [name-of-plugin]!
+
+def init(page, config):
+	# This code runs when the generate command is called.
+	print("Example plugin initialized!")
+
+def site(page):
+	# This code run whenever a new DarsSite is created.
+	print("Site created.")
+	# Doing something with the given site (printing the title)
+	print("Site title: " + page.title)
